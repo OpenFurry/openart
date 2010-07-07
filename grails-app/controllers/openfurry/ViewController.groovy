f12f4f6d9e050c04525793676af2e7beab344ebf
Matthew Scott <mjs@mjs-svc.com>
2010-07-07 17:47:49 -0600

    Little bit of work before dinner

--- grails-app/controllers/openfurry/ViewController.groovy
+++ grails-app/controllers/openfurry/ViewController.groovy
@@ -25,6 +25,7 @@ class ViewController {
                 redirect(action: 'application', id: userObject.id)
                 break
             default:
+                flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'UserObject.label', default: 'UserObject'), params.id])}"
                 response.sendError(404)
         }
     }
@@ -35,7 +36,61 @@ class ViewController {
             flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'audioUserObject.label', default: 'AudioUserObject'), params.id])}"
             response.status = 404
         } else {
-            [audioUserObjectInstance: audioUserObjectInstance]
+            [instance: audioUserObjectInstance]
+        }
+    }
+    def video = {
+        def videoUserObjectInstance = VideoUserObject.get(params.id)
+        if (!videoUserObjectInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'videoUserObject.label', default: 'VideoUserObject'), params.id])}"
+            response.status = 404
+        } else {
+            [instance: videoUserObjectInstance]
+        }
+    }
+    def flash = {
+        def flashUserObjectInstance = FlashUserObject.get(params.id)
+        if (!flashUserObjectInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'flashUserObject.label', default: 'FlashUserObject'), params.id])}"
+            response.status = 404
+        } else {
+            [instance: flashUserObjectInstance]
+        }
+    }
+    def image = {
+        def imageUserObjectInstance = ImageUserObject.get(params.id)
+        if (!imageUserObjectInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'imageUserObject.label', default: 'ImageUserObject'), params.id])}"
+            response.status = 404
+        } else {
+            [instance: imageUserObjectInstance]
+        }
+    }
+    def text = {
+        def textUserObjectInstance = TextUserObject.get(params.id)
+        if (!textUserObjectInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'textUserObject.label', default: 'TextUserObject'), params.id])}"
+            response.status = 404
+        } else {
+            [instance: textUserObjectInstance]
+        }
+    }
+    def journal = {
+        def journalUserObjectInstance = TextUserObject.get(params.id)
+        if (!journalUserObjectInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'journalUserObject.label', default: 'TextUserObject'), params.id])}"
+            response.status = 404
+        } else {
+            [instance: journalUserObjectInstance]
+        }
+    }
+    def application = {
+        def applicationUserObjectInstance = ApplicationUserObject.get(params.id)
+        if (!applicationUserObjectInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'applicationUserObject.label', default: 'ApplicationUserObject'), params.id])}"
+            response.status = 404
+        } else {
+            [instance: applicationUserObjectInstance]
         }
     }
 }
--- grails-app/views/view/journal.gsp
+++ grails-app/views/view/journal.gsp
@@ -7,6 +7,5 @@
         <div class="audioDisplay">
             <a href="${createLinkTo(dir: 'submissions/audio/' + audioUserObjectInstance.owner.username, file: audioUserObjectInstance.file)}">AUDIO FILE</a>
         </div>
-        <g:render template="uo" />
     </body>
 </html
