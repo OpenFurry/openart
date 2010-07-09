88a170c09d0710108fb3fc0b81fc73bf947aa675
Matthew Scott <mjs@mjs-svc.com>
2010-07-09 00:34:15 -0600

    Of course I forgot something...

--- grails-app/controllers/openfurry/LicenseController.groovy
+++ grails-app/controllers/openfurry/LicenseController.groovy
@@ -3,17 +3,4 @@ package openfurry
 class LicenseController {
 
     def scaffold = License
-
-    def messagingService
-    def authenticateService
-
-    def mtest = {
-        def m = new UserMessage(
-            recipient: authenticateService.userDomain(),
-            message: "Test!",
-            type: 'error'
-            )
-        m.save(flush: true)
-        redirect(uri: '/')
-    }
 }
